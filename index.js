1246a6df0b2a159e8d90c130a2741d39f8fb2c92
melika <mlikaghasabnezhad82@gmail.com>
2025-05-17 18:58:01 +0330

    add fitcher

--- index.js
+++ index.js
@@ -54,24 +54,31 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-const displayMovements = function (movements, sort = false) {
-  containerMovements.innerHTML = '';
-
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
-
-  movs.forEach(function (mov, i) {
+//show html elements
+const displayMovements = function (movements) {
+    containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `<div class="movements__row">
+<div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+ 
+<div class="movements__value">${mov}
+</div>`;
 
-    const html = `
-      <div class="movements__row">
-        <div class="movements__type movements__type--${type}">${
-      i + 1
-    } ${type}</div>
-        <div class="movements__value">${mov}€</div>
-      </div>
-    `;
-
-    containerMovements.insertAdjacentHTML('afterbegin', html);
+containerMovements.insertAdjacentHTML('afterbegin' , html);
   });
 };
+
 displayMovements(account1.movements);
+
+//SET USERNAME
+
+const creatUsernames = function(accounts){
+  accounts.forEach(function(account){
+    account.username = account.owner.toLowerCase().split(' ').map(name => name[0]).join('');
+  });
+  return accounts; // آرایه accounts را برگردانید
+};
+
+const result = creatUsernames(accounts);
+console.log(result); // آرایه accounts با username های اضافه شده
